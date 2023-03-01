ed32e8a8bc59860b9dbbbdf35d4967fcbf9b6d43
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2023-03-01 13:04:02 +0300

    anime

--- src/Components/Blog/BlogNav.js
+++ src/Components/Blog/BlogNav.js
@@ -8,7 +8,7 @@ function BlogNav() {
     <div className='BlogNav flex items-center  h-full'>
 <div className='flex items-center justify-between w-screen md:w-5/6 mx-auto'>
 <Link to='/'> <Typography className = 'fitbitName'>Better You<span className='period'>.</span></Typography></Link>
-<div className='Welcome-blog-text'> <p>Welcome To Our Blog !</p></div>
+<div className='animate__animated animate__bounceInUp animate__repeat-2'> <p className='text-green-600'>Welcome To Our Blog !</p></div>
 </div>
     </div>
   )
