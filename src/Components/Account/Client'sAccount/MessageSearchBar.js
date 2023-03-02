e36634097b3f5f4760997ce7aa07331681056f13
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2023-03-02 20:24:52 +0300

    ..

--- src/Components/Account/Client'sAccount/MessageSearchBar.js
+++ src/Components/Account/Client'sAccount/MessageSearchBar.js
@@ -5,7 +5,7 @@ function MessageSearchBar() {
   return (
     <div className='w-full pb-3 grid place-content-center '>
         <div className='flex justify-between items-center place-content-center border px-1 rounded-full '>
-        <input  type='text' className='w-full'/>
+        <input  type='text' className='w-full bg-white'/>
         <p>
             <ImSearch  className='md:text-xl text-green-600 mx-2' />
         </p>
@@ -14,4 +14,4 @@ function MessageSearchBar() {
   )
 }
 
-export default MessageSearchBar
+export default MessageSearchBar;
