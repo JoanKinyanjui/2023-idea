31d21c8b27b9b83566cdb40b3e514efcff78288a
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2023-03-02 20:22:52 +0300

    simple responsiveness changes

---
+++ src/Components/Account/Client'sAccount/MessageSearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {ImSearch} from 'react-icons/im'
+
+function MessageSearchBar() {
+  return (
+    <div className='w-full pb-3 grid place-content-center '>
+        <div className='flex justify-between items-center place-content-center border px-1 rounded-full '>
+        <input  type='text' className='w-full'/>
+        <p>
+            <ImSearch  className='md:text-xl text-green-600 mx-2' />
+        </p>
+        </div>
+    </div>
+  )
+}
+
+export default MessageSearchBar
--- src/Components/Account/Client'sAccount/Messages.js
+++ src/Components/Account/Client'sAccount/Messages.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './ClientAccount.css'
+import MessageSearchBar from './MessageSearchBar';
 
 function Messages() {
   return (
     <div className='Message'>
+      <div>
+        <MessageSearchBar />
+      </div>
 <Link to='/chatroom' >
-<div className='flex items-center justify-between py-3 border-b py-1 border-t'>
+<div className='flex items-center justify-between py-3 border-b py-1 '>
        <div className='flex '>
        <img src='/images/pexels-biola-visuals-15122857.jpg' className='Messages-img'/>
        <div className='grid px-3 '>
@@ -18,6 +22,43 @@ function Messages() {
 <div className='grid justify-items-center'>
   <div className='message-time'>17:20</div>
 <div className='message-no flex items-center place-content-center text-no'>2</div>
+</div>
+      </div>
+</Link>
+
+
+{/* Two */}
+<Link to='/chatroom' >
+<div className='flex items-center justify-between py-3 border-b py-1 '>
+       <div className='flex '>
+       <img src='https://images.pexels.com/photos/1367278/pexels-photo-1367278.jpeg?auto=compress&cs=tinysrgb&w=1600' className='Messages-img'/>
+       <div className='grid px-3 '>
+       <p className='flex justify-start'>Dr. Jane Gakenia</p>
+       <p className='flex  justify-start text-gray-500 text-sm'>I hope you have been sleeping fine.</p>
+       </div>
+       </div>
+       
+<div className='grid justify-items-center'>
+  <div className='message-time'>11:00</div>
+{/* <div className='message-no flex items-center place-content-center text-no'></div> */}
+</div>
+      </div>
+</Link>
+
+{/* Three */}
+<Link to='/chatroom' >
+<div className='flex items-center justify-between py-3 border-b py-1 '>
+       <div className='flex '>
+       <img src='https://images.pexels.com/photos/6212656/pexels-photo-6212656.jpeg?auto=compress&cs=tinysrgb&w=1600' className='Messages-img'/>
+       <div className='grid px-3 '>
+       <p className='flex justify-start'>Dr. Grace Kimando</p>
+       <p className='flex  justify-start text-gray-500 text-sm'>Are you coping Okay.</p>
+       </div>
+       </div>
+       
+<div className='grid justify-items-center'>
+  <div className='message-time'>08:40</div>
+<div className='message-no flex items-center place-content-center text-no'>1</div>
 </div>
       </div>
 </Link>
