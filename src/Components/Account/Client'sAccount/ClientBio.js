9fb0149d8a5007e4f3fcd4434b5c310fb9be4836
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2023-03-07 10:14:01 +0300

    padding

--- src/Components/Account/Client'sAccount/ClientBio.js
+++ src/Components/Account/Client'sAccount/ClientBio.js
@@ -8,13 +8,13 @@ import { Typography } from '@material-ui/core';
 function ClientBio() {
   return (
     <div className='ClientBio flex items-center grid'>
-        <div className='ClientBio-nav w-5/6 place-self-center mx-auto flex justify-between text-green-900 py-2 px-2 md:text-xl items-center'>
+        <div className='ClientBio-nav w-11/12 md:w-5/6 place-self-center mx-auto flex justify-between text-green-900 py-2 px-2 md:text-xl items-center pb-4 md:pb-2'>
         <Link to='/'><p><MdArrowBackIosNew /></p></Link>
         <Link to='/'> <Typography className = 'fitbitName'>Better You<span className='period'>.</span></Typography></Link>
         </div>
         <div className='grid mx-auto borderrounded-full '>
         <img  src='https://images.pexels.com/photos/4052752/pexels-photo-4052752.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='ClientBio-img '/>
-         <p className='mx-auto w-full'><MdEdit /></p>
+         {/* <p className='mx-auto w-full'><MdEdit /></p> */}
         <div className='ClientBio-content'>
         <p className='py-1' >Joan Kinyanjui</p>
         <p className='py-1'>kinyanjuijoan52@gmail.com</p>
